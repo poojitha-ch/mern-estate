f6f4641fd5eff21eac6be6bd3e63c3d61fe34c36
poojitha-ch <poojithachalla.2004@gmail.com>
2024-02-16 20:12:29 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Singout from "./pages/Singout";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/signin";
+import Header from "./components/Header";
 
 
 export default function App() {
   return (
    <BrowserRouter>
+   <Header/>
    <Routes>
     <Route path='/' element={<Home/>} />
     <Route path='/sign-in' element={<SignIn/>} />
